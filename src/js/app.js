96de980ca4b98053329f6a4189ef0b611669fedd
junaer <junaers1996@gmail.com>
2023-05-25 15:16:40 +0300

    finish game

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,50 @@
 // TODO: write code here
+
+let counter = document.querySelector('.counter')
+let miss = document.querySelector('.miss')
+
+let holes = document.querySelectorAll('.hole')
+let activeHole = document.querySelector('.active')
+
+ holes.forEach(element => {
+    element.addEventListener('click', () => {
+        if (element.classList[2]) {
+            counter.textContent++
+        }else{
+            miss.textContent++
+        }
+        
+    })
+ });
+
+function getRandomArbitrary() {
+    let int = Math.random() * (16 - 2) + 1;
+    return Math.round(int)
+  }
+
+function play (start) {
+    if (miss.textContent < 10 && counter.textContent < 10) {
+        let activeHole = document.querySelector('.active')
+        activeHole.classList.toggle('active') 
+        activeHole.classList.add('hole')
+        let numberElement = getRandomArbitrary()
+        let nextActive = holes[numberElement]
+        nextActive.classList.toggle('active')
+    }else if (miss.textContent >= 10) {
+        clearInterval(start)
+        alert('lose')
+        counter.textContent = 0
+        miss.textContent = 0
+        start
+    }else if (counter.textContent >= 10) {
+        clearInterval(start)
+        alert('win')
+        counter.textContent = 0
+        miss.textContent = 0
+        start
+    } 
+}
+
+let start = setInterval(play, 1500)
+
+play(start)
